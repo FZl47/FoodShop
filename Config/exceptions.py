6fe7171771bf4590a05a8511684919f6a8ba9cfe
FZl47 <fzl8747@gmail.com>
2022-07-15 06:21:20 +0430

    Add in exceptions and permissions

--- Config/exceptions.py
+++ Config/exceptions.py
@@ -52,6 +52,10 @@ class Problem(APIException):
     status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     default_detail = ResponseDict(status_code,message='اوه ، مشکلی پیش امده است ...',error='Ohh , There is a Problem')
 
+class ProblemAddToCart(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = ResponseDict(status_code,'مشکلی در اضافه کردن محصول به سبد خرید وجود دارد',error='Cant add to cart')
+
 class NotFound(APIException):
     status_code = status.HTTP_404_NOT_FOUND
     default_detail = ResponseDict(status_code,'چیزی یافت نشد',error='Not found')
--- Config/permissions.py
+++ Config/permissions.py
@@ -2,10 +2,13 @@ from rest_framework.permissions import BasePermission
 from Config import exceptions
 
 
-
 class IsAuthenticated(BasePermission):
     def has_permission(self, request, view):
         if bool(request.user and request.user.is_authenticated):
             return True
         raise exceptions.NeedLogin()
 
+
+class AllowAny(BasePermission):
+    def has_permission(self, request, view):
+        return True
