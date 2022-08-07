5b6ad86b4f84e690411f9fe810e68507b55cb2f0
FZl47 <wwwfazel88@gmail.com>
2022-08-07 16:59:24 +0430

    Add User model in admin

--- User/admin.py
+++ User/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from . import models
 
+admin.site.register(models.User)
 admin.site.register(models.Order)
 admin.site.register(models.OrderDetail)
 admin.site.register(models.Address)
